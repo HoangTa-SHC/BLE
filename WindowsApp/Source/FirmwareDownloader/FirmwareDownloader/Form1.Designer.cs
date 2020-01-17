7293f395e89b11449e25ce65f338d2801fa40059
Anh Tran <anh.tran@swhwc.com>
2020-01-17 13:49:55 +0700

    Add Windows App

---
+++ WindowsApp/Source/FirmwareDownloader/FirmwareDownloader/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace FirmwareDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbSerialNo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(261, 141);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 57);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbSerialNo
+            // 
+            this.lbSerialNo.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSerialNo.ForeColor = System.Drawing.Color.Green;
+            this.lbSerialNo.Location = new System.Drawing.Point(12, 44);
+            this.lbSerialNo.Name = "lbSerialNo";
+            this.lbSerialNo.Size = new System.Drawing.Size(607, 57);
+            this.lbSerialNo.TabIndex = 2;
+            this.lbSerialNo.Text = "0000000";
+            this.lbSerialNo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 242);
+            this.Controls.Add(this.lbSerialNo);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Firmware Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbSerialNo;
+    }
+}
+
---
+++ WindowsApp/Source/FirmwareDownloader/FirmwareDownloader/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IniParser;
+using IniParser.Model;
+
+namespace FirmwareDownloader
+{
+    public partial class Form1 : Form
+    {
+        struct SerialNumber
+        {
+            public UInt64 start;
+            public UInt64 max;
+            public Byte length;
+            public UInt64 address;
+        }
+        SerialNumber serial_number;
+        const string CONFIGURATION_FILE = "settings.ini";
+        const string JLINK_TEMPLATE_FILE = "script.jlink";
+        const string JLINK_SCRIPT_FILE = "script.jlink.tmp";
+        const string LOG_FILE = "logs.txt";
+        IniData config;
+        string jlink;
+        Boolean force = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            
+            if (serial_number.max > 0)
+            {
+                // Do work
+                string jlink_script = File.ReadAllText(JLINK_TEMPLATE_FILE);
+                string serial_string = serial_number.start.ToString("X" + serial_number.length);
+                byte[] serial_hex = Encoding.Default.GetBytes(serial_string);
+                File.WriteAllText(JLINK_SCRIPT_FILE, jlink_script);
+                for (Byte i = 0; i < serial_number.length; i++)
+                {
+                    File.AppendAllText(JLINK_SCRIPT_FILE, "\r\nw1 0x" + (serial_number.address + i).ToString("X") + ", 0x" + serial_hex[i].ToString("X2"));
+                }
+                if (force)
+                {
+                    File.AppendAllText(JLINK_SCRIPT_FILE, "\r\nr\r\n");
+                }
+                File.AppendAllText(JLINK_SCRIPT_FILE, "\r\nexit\r\n");
+
+
+                // Execute the command synchronously.
+                ExecuteCommand exe = new ExecuteCommand();
+                string result = exe.ExecuteCommandSync(jlink + " -CommanderScript " + JLINK_SCRIPT_FILE);
+                File.AppendAllText(LOG_FILE, result);
+                File.Delete(JLINK_SCRIPT_FILE);
+
+                if (result.Contains("Connecting to J-Link via USB...FAILED: Cannot connect to J-Link via USB."))
+                {
+                    MessageBox.Show("Cannot connect to J-Link via USB!");
+                }
+
+                if (result.Contains("Cannot connect to target."))
+                {
+                    MessageBox.Show("Cannot connect to target!");
+                }
+
+                // Update ini file
+                serial_number.start++;
+                serial_number.max--;
+                config["SerialNo"]["Start"] = serial_number.start.ToString("X" + serial_number.length);
+                config["SerialNo"]["Max"] = serial_number.max.ToString();
+                var parser = new FileIniDataParser();
+                parser.WriteFile(CONFIGURATION_FILE, config);
+
+                lbSerialNo.Text = serial_number.start.ToString("X" + serial_number.length);
+                btnStart.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Maximum count reached!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var parser = new FileIniDataParser();
+            if (File.Exists(CONFIGURATION_FILE))
+            {
+                config = parser.ReadFile(CONFIGURATION_FILE);
+                serial_number.start = Convert.ToUInt64(config["SerialNo"]["Start"], 16);
+                serial_number.max = UInt64.Parse(config["SerialNo"]["Max"]);
+                serial_number.length = Byte.Parse(config["SerialNo"]["Length"]);
+                serial_number.address = Convert.ToUInt64(config["SerialNo"]["Address"], 16);
+                Console.WriteLine("Address" + serial_number.address);
+                jlink = config["System"]["JLINK"];
+                force = Convert.ToBoolean(config["System"]["Force"]);
+                Console.WriteLine("Force:" + force);
+                if (jlink == null)
+                {
+                    jlink = "JLink";
+                }
+                lbSerialNo.Text = serial_number.start.ToString("X" + serial_number.length);
+
+                if (serial_number.max <= 0)
+                {
+                    MessageBox.Show("Maximum count reached!");
+                    btnStart.Enabled = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not find configuration file");
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+    }
+}
